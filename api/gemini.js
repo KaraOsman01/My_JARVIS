1e3934dc0571717dfdf616250d72dbb41509bcfd
Aamir Ahmad wani <133368170+KaraOsman01@users.noreply.github.com>
2026-04-09 11:03:34 +0530

    Create gemini.js

---
+++ api/gemini.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+    const API_KEY = process.env.API_KEY;
+
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Method not allowed" });
+    }
+
+    try {
+        const response = await fetch(
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-flash-lite-preview:generateContent?key=" + API_KEY,
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(req.body)
+            }
+        );
+
+        const data = await response.json();
+        res.status(200).json(data);
+
+    } catch (err) {
+        res.status(500).json({ error: "Server error" });
+    }
+}
