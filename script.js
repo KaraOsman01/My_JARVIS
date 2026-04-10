898c03c6d63c9b1c560014ea6c92444c6cd065f8
Aamir Ahmad wani <133368170+KaraOsman01@users.noreply.github.com>
2026-04-10 11:42:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,14 +40,14 @@ recognition.onresult = async (event) => {
     const transcript = event.results[event.results.length - 1][0].transcript.toLowerCase().trim();
     console.log("Heard:", transcript);
 
-    if (transcript.includes("jarvis")) {
+   /* if (transcript.includes("jarvis")) {
         let cleanCommand = transcript.split("jarvis").pop().trim();
         if (cleanCommand) {
             await handleCommand(cleanCommand);
         } else {
             speak("Yes Sir, I am listening.");
         }
-    }
+    }*/
 };
 
 // 4. Loop Prevention on End
