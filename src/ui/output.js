d4fdbc537a6d93df84e47393d3b2d68cd5c72673
Aamir Ahmad wani <133368170+KaraOsman01@users.noreply.github.com>
2026-05-06 15:02:29 +0530

    Update output.js

--- src/ui/output.js
+++ src/ui/output.js
@@ -1,1 +1,10 @@
+export function setOutput(text) {
 
+    const output = document.getElementById("output");
+
+    if (!output) return;
+
+    output.innerText = text;
+
+    output.scrollTop = output.scrollHeight;
+}
