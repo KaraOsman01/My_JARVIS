f101e5dce7e080c92577881f2d8ec857b749bf43
Aamir Ahmad wani <133368170+KaraOsman01@users.noreply.github.com>
2026-05-06 15:04:06 +0530

    Update speech.js

--- src/core/speech.js
+++ src/core/speech.js
@@ -1,1 +1,38 @@
+import { appState } from "./state.js";
 
+import { setOutput } from "../ui/output.js";
+
+import { updateVisualCoreState } from "../ui/visuals.js";
+
+export function speak(text) {
+
+    window.speechSynthesis.cancel();
+
+    const speech = new SpeechSynthesisUtterance(text);
+
+    speech.lang = "en-IN";
+
+    speech.volume = 1;
+
+    speech.rate = 1;
+
+    speech.pitch = 1;
+
+    appState.speaking = true;
+
+    updateVisualCoreState(
+        "speaking",
+        "JARVIS: " + text
+    );
+
+    setOutput("JARVIS: " + text);
+
+    speech.onend = () => {
+
+        appState.speaking = false;
+
+        updateVisualCoreState("idle");
+    };
+
+    window.speechSynthesis.speak(speech);
+}
