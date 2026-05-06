070d2afa529a23b34a9995b6c2c85f5158799119
Aamir Ahmad wani <133368170+KaraOsman01@users.noreply.github.com>
2026-05-06 15:03:12 +0530

    Update visuals.js

--- src/ui/visuals.js
+++ src/ui/visuals.js
@@ -1,1 +1,35 @@
+import { setOutput } from "./output.js";
 
+export function updateVisualCoreState(state, text = "") {
+
+    const micContainer = document.querySelector(".mic-btn-container");
+
+    if (!micContainer) return;
+
+    micContainer.classList.remove(
+        "reactive-listen",
+        "reactive-speak"
+    );
+
+    if (state === "listening") {
+
+        micContainer.classList.add("reactive-listen");
+
+        setOutput(text || "System Listening...");
+
+    }
+
+    else if (state === "speaking") {
+
+        micContainer.classList.add("reactive-speak");
+
+        setOutput(text || "Jarvis Speaking...");
+
+    }
+
+    else {
+
+        setOutput("System Idle.");
+
+    }
+}
