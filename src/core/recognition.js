ff04d5704afbd88965b468ac6234e7907bbab1bd
Aamir Ahmad wani <133368170+KaraOsman01@users.noreply.github.com>
2026-05-06 15:09:38 +0530

    Update recognition.js

--- src/core/recognition.js
+++ src/core/recognition.js
@@ -1,1 +1,46 @@
+import { handleCommand }
+from "../commands/index.js";
 
+import { updateVisualCoreState }
+from "../ui/visuals.js";
+
+const SpeechRecognition =
+    window.SpeechRecognition
+    ||
+    window.webkitSpeechRecognition;
+
+const recognition =
+    new SpeechRecognition();
+
+recognition.lang = "en-IN";
+
+recognition.continuous = false;
+
+recognition.interimResults = false;
+
+recognition.onstart = () => {
+
+    updateVisualCoreState("listening");
+};
+
+recognition.onresult = async (event) => {
+
+    const transcript =
+        event.results[0][0].transcript;
+
+    await handleCommand(transcript);
+};
+
+recognition.onend = () => {
+
+    setTimeout(() => {
+
+        recognition.start();
+
+    }, 500);
+};
+
+export function startRecognition() {
+
+    recognition.start();
+}
