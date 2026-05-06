61f8db799e9f29258e373929bc695d044a25f1b7
Aamir Ahmad wani <133368170+KaraOsman01@users.noreply.github.com>
2026-05-06 15:06:32 +0530

    Update gemini.js

--- src/ai/gemini.js
+++ src/ai/gemini.js
@@ -1,1 +1,55 @@
+import { loadMemory, saveMemory }
+from "../core/memory.js";
 
+export async function askGemini(prompt) {
+
+    const url = "/api/gemini";
+
+    let chatHistory = loadMemory();
+
+    chatHistory.push({
+        role: "user",
+        parts: [{ text: prompt }]
+    });
+
+    const requestBody = {
+
+        contents: chatHistory
+    };
+
+    try {
+
+        const res = await fetch(url, {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify(requestBody)
+        });
+
+        const data = await res.json();
+
+        const reply =
+            data.candidates?.[0]?.content?.parts?.[0]?.text
+            ||
+            "No response.";
+
+        chatHistory.push({
+            role: "model",
+            parts: [{ text: reply }]
+        });
+
+        saveMemory(chatHistory);
+
+        return reply;
+
+    } catch (err) {
+
+        console.error(err);
+
+        return "Connection error.";
+    }
+}
