4cccfb6f2f267cf431a833b9e18ea97d4a700ce8
Aamir Ahmad wani <133368170+KaraOsman01@users.noreply.github.com>
2026-05-06 15:01:15 +0530

    Update state.js

--- src/core/state.js
+++ src/core/state.js
@@ -1,1 +1,16 @@
+// GLOBAL APP STATE
 
+export const appState = {
+
+    listening: false,
+
+    speaking: false,
+
+    awake: false,
+
+    initialized: false,
+
+    currentUser: "Sir",
+
+    currentMode: "idle"
+};
