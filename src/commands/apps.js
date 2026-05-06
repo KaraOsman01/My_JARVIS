426e221c53aab66bb87f2887c0d6da04afac2f92
Aamir Ahmad wani <133368170+KaraOsman01@users.noreply.github.com>
2026-05-06 15:07:49 +0530

    Implement functions to open YouTube and Google
    Added functions to open YouTube and Google with speech feedback.

--- src/commands/apps.js
+++ src/commands/apps.js
@@ -1,1 +1,21 @@
+import { speak } from "../core/speech.js";
 
+export function openYoutube() {
+
+    speak("Opening YouTube");
+
+    window.open(
+        "https://youtube.com",
+        "_blank"
+    );
+}
+
+export function openGoogle() {
+
+    speak("Opening Google");
+
+    window.open(
+        "https://google.com",
+        "_blank"
+    );
+}
