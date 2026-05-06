c1c03dd5b702a18c156697b1a341e1cfdea4069b
Aamir Ahmad wani <133368170+KaraOsman01@users.noreply.github.com>
2026-05-06 15:04:46 +0530

    Update memory.js

--- src/core/memory.js
+++ src/core/memory.js
@@ -1,1 +1,16 @@
+const MEMORY_KEY = "jarvis_memory";
 
+export function loadMemory() {
+
+    return JSON.parse(
+        localStorage.getItem(MEMORY_KEY)
+    ) || [];
+}
+
+export function saveMemory(memory) {
+
+    localStorage.setItem(
+        MEMORY_KEY,
+        JSON.stringify(memory)
+    );
+}
